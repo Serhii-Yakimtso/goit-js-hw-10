6abd288f933a3aaf91702668b541fdfea6ef09e1
Serhii-Yakimtso <64007661+Serhii-Yakimtso@users.noreply.github.com>
2024-02-19 18:23:13 +0000

    Deploying to gh-pages from @ Serhii-Yakimtso/goit-js-hw-10@47f256e18554573c3cc81c6b8ea1627fcbe6adb0 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-2b44ac2e.js";let o;const n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),o=e[0].getTime(),console.log(o)}};t("#datetime-picker",n);function i(){return Date.now()}console.log(i());
+//# sourceMappingURL=commonHelpers.js.map
