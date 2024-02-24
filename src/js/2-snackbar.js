00294150974c8f5bcd7a589e113dd8dea2a45e51
Serhii-Yakimtso <serafyso@gmail.com>
2024-02-25 00:03:11 +0200

    finish edit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,83 +2,17 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import errorIcon from '../img/error.svg';
-import cautionIcon from '../img/caution.svg';
 import okIcon from '../img/ok.svg';
 
 const form = document.querySelector('.form');
-const inputDelay = document.querySelector('input[name="delay"]');
-const inputFulfilled = document.querySelector('input[value="fulfilled"]');
-const inputRejected = document.querySelector('input[value="rejected"]');
-const btnSubmit = document.querySelector('button');
 
-inputFulfilled.addEventListener('click', addInputFulfilledChecked);
-inputRejected.addEventListener('click', addInputRejectedChecked);
-btnSubmit.addEventListener('click', handlePromiseGenerator);
-
-// функції зміни статусу checked на радіо-кнопках
-function addInputFulfilledChecked() {
-  inputFulfilled.setAttribute('checked', '');
-  inputRejected.removeAttribute('checked');
-}
-
-function addInputRejectedChecked() {
-  inputRejected.setAttribute('checked', '');
-  inputFulfilled.removeAttribute('checked');
-}
-
-// функція прийняття delay із форми
-function getDelay() {
-  if (!inputDelay.value) {
-    iziToast.show({
-      title: 'Caution',
-      titleColor: '#fff',
-      titleSize: '16px',
-
-      message: 'Enter the delay',
-      messageColor: '#fff',
-      messageSize: '16px',
-
-      iconUrl: cautionIcon,
-
-      position: 'topRight',
-      backgroundColor: '#ffa000',
-    });
-    return;
-  }
-
-  return inputDelay.value;
-}
-
-// функція визначення стану із форми
-function getPromiseState() {
-  if (document.querySelector('[checked]') === null) {
-    iziToast.show({
-      title: 'Caution',
-      titleColor: '#fff',
-      titleSize: '16px',
-
-      message: 'Choose a state',
-      messageColor: '#fff',
-      messageSize: '16px',
-
-      iconUrl: cautionIcon,
-
-      position: 'topRight',
-      backgroundColor: '#ffa000',
-    });
-    return;
-  }
-
-  return document.querySelector('[checked]').getAttribute('value');
-}
-
-// функція виклику промісу
+form.addEventListener('submit', handlePromiseGenerator);
 
 function handlePromiseGenerator(e) {
   e.preventDefault();
 
-  const delay = getDelay();
-  const state = getPromiseState();
+  const delay = form.elements.delay.value;
+  const state = form.elements.state.value;
 
   const makePromise = (delayValue, stateValue) => {
     return new Promise((resolve, reject) => {
@@ -100,7 +34,7 @@ function handlePromiseGenerator(e) {
         titleColor: '#fff',
         titleSize: '16px',
 
-        message: ` Fulfilled promise in ${delay}ms`,
+        message: `Fulfilled promise in ${delay}ms`,
         messageColor: '#fff',
         messageSize: '16px',
 
@@ -109,6 +43,7 @@ function handlePromiseGenerator(e) {
         position: 'topRight',
         backgroundColor: '#59a10d',
       });
+      console.log(`Fulfilled promise in ${delay}ms`);
     })
     .catch(error => {
       iziToast.show({
@@ -116,7 +51,7 @@ function handlePromiseGenerator(e) {
         titleColor: '#fff',
         titleSize: '16px',
 
-        message: ` Rejected promise in ${delay}ms`,
+        message: `Rejected promise in ${delay}ms`,
         messageColor: '#fff',
         messageSize: '16px',
 
@@ -125,5 +60,6 @@ function handlePromiseGenerator(e) {
         position: 'topRight',
         backgroundColor: '#ef4040',
       });
+      console.log(`Rejected promise in ${delay}ms`);
     });
 }
